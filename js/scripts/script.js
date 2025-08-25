97187abf60109f78d04474389c07615d3461aacd
Davi Fernandes <davi.fer159@gmail.com>
2025-08-25 19:55:17 -0300

    section hero finalizada

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,5 +1,17 @@
-const sayHello = () => {
-  return "Hellow";
-};
+const slide_thumbnail = new Swiper(".slide-thumbnail", {
+  slidesPerView: 5,
+  direction: "vertical",
+  spaceBetween: 20,
+  watchSlidesProgress: true,
+});
 
-const result = sayHello();
+const slide_hero = new Swiper(".slide-principal", {
+  effect: "fade",
+  thumbs: {
+    swiper: slide_thumbnail,
+  },
+  autoplay: {
+    delay: 5000,
+    disabledOnInteraction: false,
+  },
+});
